4ce88a2a2a985e437b16bd4090575529b0225666
elnarasyazani <elnara.syazani@gmail.com>
2024-06-16 22:41:34 +0700

    Update page.tsx

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return <div className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20"><SignIn /></div>;
 }
